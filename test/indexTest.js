10205d57607c4223239aa3e0aaa519d55b7abf27
oliverfeher <oliver92.feher@gmail.com>
2020-04-13 01:05:57 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,4 +3,8 @@ describe("index.js", () => {
     expect(testVar).to.exist
     expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
   })
-})
+});
+
+it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});
